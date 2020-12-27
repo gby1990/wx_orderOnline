359749630539065551349011fb165129b6e71462
gby1990 <36633416+gby1990@users.noreply.github.com>
2020-12-27 22:22:34 +0100

    bug-首页了解更多导航无法跳转到健康课程页面

--- pages/class/myprod/product.js
+++ pages/class/myprod/product.js
@@ -1,4 +1,6 @@
-// pages/class/myprod/product.js
+// pages/lesssons/xunjiu.js
+const app = getApp();
+
 Page({
 
   /**
@@ -6,6 +8,43 @@ Page({
    */
   data: {
 
+		//scrollview 高度
+		windowHeight: 0,    //高度
+		currentTab: 0,	 	//导航栏指向
+		cart: [],			//购物车
+		cartTotal: 0,		//购物车件数
+		cartTotalPrice: 0,   //购物车价格总计
+		foodList: [            // 分类
+			{
+				id: 0,
+        name: '熏灸理疗',
+    
+			},
+			{
+				id: 1,
+				name: '艾灸理疗'
+			},
+			{
+				id: 2,
+				name: '艾草日化'
+			},
+    ],
+    imgUrls:[
+      'https://pic.downk.cc/item/5fe8f58c3ffa7d37b316051a.jpg',
+      'https://pic.downk.cc/item/5fe8f5d23ffa7d37b31616fe.jpg',
+      'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b1.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b3.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b6.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b9.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922bc.jpg',
+     'https://pic.downk.cc/item/5fe663db3ffa7d37b3a955c9.jpg',
+     'https://pic.downk.cc/item/5fe8fc8c3ffa7d37b317d3c3.jpg',
+     'https://pic.downk.cc/item/5fe8ff513ffa7d37b318862a.jpg',
+     'https://pic.downk.cc/item/5fe8ff513ffa7d37b318862c.jpg',
+     'https://pic.downk.cc/item/5fe8ff513ffa7d37b318862f.jpg',
+     'https://pic.downk.cc/item/5fe8ff513ffa7d37b3188634.jpg',
+
+    ],
   },
 
   /**
