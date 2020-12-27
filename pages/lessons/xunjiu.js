c847383fee815c9a2b6e3d41d26d3e86ee0b7b09
gby1990 <36633416+gby1990@users.noreply.github.com>
2020-12-27 17:04:00 +0100

    健康课程页

---
+++ pages/lessons/xunjiu.js
@@ -0,0 +1,98 @@
+// pages/lesssons/xunjiu.js
+const app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+		//scrollview 高度
+		windowHeight: 0,    //高度
+		currentTab: 0,	 	//导航栏指向
+		cart: [],			//购物车
+		cartTotal: 0,		//购物车件数
+		cartTotalPrice: 0,   //购物车价格总计
+		foodList: [            // 分类
+			{
+				id: 0,
+				name: '熏灸理疗'
+			},
+			{
+				id: 1,
+				name: '艾灸理疗'
+			},
+			{
+				id: 2,
+				name: '艾草日化'
+			},
+
+    ],
+    imgUrls:[
+      'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b1.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b3.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b6.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922b9.jpg',
+     'https://pic.downk.cc/item/5fe663523ffa7d37b3a922bc.jpg',
+     'https://pic.downk.cc/item/5fe663db3ffa7d37b3a955c9.jpg',
+     'https://pic.downk.cc/item/5fe6738a3ffa7d37b3ae6001.jpg',
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
